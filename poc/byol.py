f022ac64cd4602fc4d685e2161bbf2f310cbeed2
Yoran Gyselen <yoran.gyselen.ext@exail.com>
2025-05-13 16:39:13 +0200

    Refined BYOL implementation

--- poc/byol.py
+++ poc/byol.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
+# Imports
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -5,21 +8,16 @@ import torchvision.models as models
 from torchvision import transforms
 from data.byol_dataset import BYOLDataset
 from torch.utils.data import DataLoader
-from data.labeled_dataset import LabeledDataset
-from data.fixmatch_dataset import FixMatchDataset
 from datetime import datetime
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from utils.create_model_dir import create_model_dir
 from utils.early_stopping import EarlyStopping
-from utils.evaluate_map import evaluate_map
 from utils.logger import Logger
-from utils.resize_with_aspect import resize_with_aspect
 import os
-import torch
 
 class MLPHead(nn.Module):
-    def __init__(self, in_dim, hidden_dim=4096, out_dim=256):
+    def __init__(self, in_dim=512, hidden_dim=1024, out_dim=256):
         super().__init__()
         self.net = nn.Sequential(
             nn.Linear(in_dim, hidden_dim),
@@ -35,7 +33,7 @@ class BYOL(nn.Module):
     def __init__(self, backbone):
         super().__init__()
         self.encoder = backbone
-        self.projector = MLPHead(in_dim=2048) # Adjust based on encoder
+        self.projector = MLPHead(in_dim=backbone.out_dim)
         self.predictor = MLPHead(in_dim=256, out_dim=256)
     
     def forward(self, x):
@@ -45,6 +43,20 @@ class BYOL(nn.Module):
         p = self.predictor(z)
         return p, z.detach()
 
+class ResNetEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        resnet = models.resnet18()
+        self.feature_extractor = nn.Sequential(*list(resnet.children())[:-2])  # Remove avgpool and fc
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.out_dim = resnet.fc.in_features
+    
+    def forward(self, x):
+        x = self.feature_extractor(x)
+        x = self.avgpool(x)
+        return x.view(x.size(0), -1)
+
+@torch.no_grad()
 def update_ema(target_net, online_net, beta=0.99):
     for target_param, online_param in zip(target_net.parameters(), online_net.parameters()):
         target_param.data = beta * target_param.data + (1. - beta) * online_param.data
@@ -64,18 +76,25 @@ train_transform = transforms.Compose([
 ])
 
 def get_models(device):
-    resnet = models.resnet50()
-    resnet.fc = nn.Identity()
-    online_net = BYOL(resnet)
-    target_net = BYOL(resnet)
+    encoder = ResNetEncoder()
+    online_net = BYOL(encoder).to(device)
+    target_net = BYOL(encoder).to(device)
     target_net.load_state_dict(online_net.state_dict())
-    return online_net.to(device), target_net.to(device)
+
+    # Freeze target network parameters
+    for param in target_net.parameters():
+        param.requires_grad = False
+
+    return online_net, target_net
 
 def train_byol_backbone(train_loader, online_net, target_net, device, epochs=100):
     optimizer = torch.optim.Adam(online_net.parameters(), lr=3e-4)
+    early_stopping = EarlyStopping(patience=10, delta=0.001, mode="min")
 
     # Training loop
     for epoch in range(epochs):
+        running_loss = 0
+        online_net.train()
         for view1, view2 in tqdm(train_loader, desc=f"Epoch {epoch+1}/{epochs}"):
             view1, view2 = view1.to(device), view2.to(device)
 
@@ -95,6 +114,19 @@ def train_byol_backbone(train_loader, online_net, target_net, device, epochs=100
             optimizer.step()
 
             update_ema(target_net, online_net, beta=0.99)
+            running_loss += loss.item()
+        
+        avg_loss = running_loss / len(train_loader)
+        logger.log(f"Epoch {epoch+1}/{epochs} | Average loss: {avg_loss:.4f}")
+
+        # Early stopping
+        early_stopping(avg_loss, online_net)
+        if early_stopping.early_stop:
+            logger.log(f"Early stopping at epoch {epoch+1}")
+            break
+
+    return early_stopping.load_best_model(online_net)
+
 
 # ====== Safe Entry Point ======
 if __name__ == "__main__":
@@ -118,26 +150,19 @@ if __name__ == "__main__":
 
     # Datasets
     train_dataset = BYOLDataset(image_dir=os.path.join(UATD_PATH, "UATD_Training", "images"), transforms=train_transform)
-    test_dataset = BYOLDataset(image_dir=os.path.join(UATD_PATH, "UATD_Test_1", "images"), transforms=train_transform)
     val_dataset = BYOLDataset(image_dir=os.path.join(UATD_PATH, "UATD_Test_2", "images"), transforms=train_transform)
 
     # DataLoaders
-    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=lambda x: tuple(zip(*x)))
-    val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, collate_fn=lambda x: tuple(zip(*x)))
-    test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False, collate_fn=lambda x: tuple(zip(*x)))
+    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
+    # Models
     online_net, target_net = get_models(DEVICE)
-    
-    model = load_pretrained_model(num_classes=NUM_CLASSES, model_path=BACKBONE_PATH, device=DEVICE)
-    model = train_fixmatch(model=model, data_loader=train_loader, device=DEVICE, logger=logger, epochs=EPOCHS)
+    train_byol_backbone(train_loader=train_loader, online_net=online_net, target_net=target_net, device=DEVICE, epochs=EPOCHS)
 
     # Export Model
-    torch.save(model.state_dict(), model_path)
+    torch.save(online_net.encoder.state_dict(), model_path)
     logger.log(f"Model saved at '{model_path}'")
-
-    # Evaluate mAP
-    eval_result = evaluate_map(model=model, data_loader=test_loader, device=DEVICE)
-    logger.log(eval_result)
 
     # Final timing
     total_time = datetime.now() - start_time
